47eddb1be89d96499ff7dbfd11c630fee489ce84
Fernando Mendes <fernando.mendes@myprofitweb.com>
2025-03-22 13:31:15 -0300

    [Update] - Alteração de versão

--- Marraia.SqlServer/Configurations/DapperConfigurationsExtensions.cs
+++ Marraia.SqlServer/Configurations/DapperConfigurationsExtensions.cs
@@ -2,17 +2,14 @@
 using Marraia.SqlServer.Uow.Interfaces;
 using Marraia.SqlServer.Uow.Transactions;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Text;
 
 namespace Marraia.SqlServer.Configurations
 {
     public static class DapperConfigurationsExtensions
     {
-        public static IServiceCollection AddMarraiaSqlServer(this IServiceCollection service, string connectionString)
+        public static IServiceCollection AddSqlServer(this IServiceCollection service, string connectionString)
         {
             service.AddScoped<IDbConnection>(db => new SqlConnection(connectionString));
             service.AddScoped<IUnitOfWork, UnitOfWork>();
